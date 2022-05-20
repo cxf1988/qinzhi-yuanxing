a79d7898fa6326cedbf0e343fd90f093a0392f79
cxf1988 <421143460@qq.com>
2022-05-20 11:59:46 +0800

    地图引入

--- src/views/MapView.vue
+++ src/views/MapView.vue
@@ -1,28 +1,69 @@
 <template>
-  <div class="map-container">
+  <div class="map-container" id="mapContainer">
     <div class="map-header-container">
       <ul class="map-title-items">
-        <li class="map-title-item">实时监测与报警</li>
+        <li class="map-title-item map-title-item-active">实时监测与报警</li>
         <li class="map-title-item">双重预防机制</li>
         <li class="map-title-item">员工在岗在位</li>
         <li class="map-title-item">安全生产全流程</li>
       </ul>
     </div>
-    <div class="map-content-container"></div>
+    <div class="map-left-content-container"></div>
+    <div class="map-top-bar-container"></div>
+    <map-bottom-bar></map-bottom-bar>
+    <div class="map-right-content-container"></div>
   </div>
 </template>
 
 <script>
+import AMapLoader from '@amap/amap-jsapi-loader'
+
+import MapBottomBar from './mapMoudle/MapBottomBar.vue'
+
 export default {
   name: 'MapView',
+  components: {
+    MapBottomBar,
+  },
   data() {
     return {}
+  },
+  beforeCreate() {
+    AMapLoader.load({
+      key: '6fb18acf97b73eaccca30a1825af0230', // 申请好的Web端开发者Key，首次调用 load 时必填
+      version: '1.4.15', // 指定要加载的 JSAPI 的版本，缺省时默认为 1.4.15
+      plugins: [], // 需要使用的的插件列表，如比例尺'AMap.Scale'等
+      AMapUI: {
+        // 是否加载 AMapUI，缺省不加载
+        version: '1.1', // AMapUI 缺省 1.1
+        plugins: [], // 需要加载的 AMapUI ui插件
+      },
+      Loca: {
+        // 是否加载 Loca， 缺省不加载
+        version: '1.3.2', // Loca 版本，缺省 1.3.2
+      },
+    })
+      .then((AMap) => {
+        this.$nextTick(() => this.initMap(AMap))
+      })
+      .catch((e) => {
+        console.error(e)
+      })
   },
   created() {},
   // mounted () {},
   computed: {},
   watch: {},
-  methods: {},
+  methods: {
+    initMap(AMap) {
+      this.map = new AMap.Map('mapContainer', {
+        zoom: 12,
+        center: [121.84408, 29.90069],
+      })
+      // 或者使用 $refs 获取节点
+      // this.map = new AMap.Map(this.$refs.container);
+    },
+  },
 }
 </script>
 
@@ -30,28 +71,61 @@ export default {
 .map-container {
   width: 100%;
   height: 100%;
+  position: relative;
   .map-header-container {
     width: 100%;
-    height: 68px;
+    height: 56px;
     background: rgba(2, 11, 45, 0.8);
     padding: 8px 0;
+    position: absolute;
+    top: 0;
+    left: 0;
+    z-index: 9999;
     display: flex;
     justify-content: center;
     .map-title-items {
       width: 800px;
-      height: 100%;
+      height: 40px;
       display: flex;
       justify-content: space-around;
       .map-title-item {
+        height: 40px;
+        line-height: 40px;
+        padding: 0 16px;
         color: #fff;
-        line-height: 52px;
+        border: 1px solid #fff;
+      }
+      .map-title-item-active {
+        box-shadow: inset 0 0 10px 0 rgb(36 165 255 / 66%);
       }
     }
   }
-  .map-content-container {
-    width: 100%;
-    height: calc(100% - 52px);
-    background: #ccc;
+  .map-left-content-container {
+    width: 300px;
+    height: calc(100% - 88px);
+    background: rgba(2, 11, 45, 0.8);
+    position: absolute;
+    top: 72px;
+    left: 16px;
+    z-index: 9999;
+  }
+  .map-top-bar-container {
+    width: 600px;
+    height: 40px;
+    background: rgba(2, 11, 45, 0.8);
+    position: absolute;
+    top: 72px;
+    left: 332px;
+    z-index: 9999;
+  }
+  .map-right-content-container {
+    width: 600px;
+    height: calc(100% - 88px);
+    background: rgba(2, 11, 45, 0.8);
+    position: absolute;
+    top: 72px;
+    right: 16px;
+    z-index: 9999;
   }
 }
 </style>
---
+++ src/views/mapMoudle/MapBottomBar.vue
@@ -0,0 +1,94 @@
+<template>
+  <div class="map-bottom-bar-container">
+    <ul class="map-bottom-bar-items">
+      <li
+        class="map-bottom-bar-item"
+        v-for="(item, index) in barData"
+        :key="index"
+      >
+        <p>{{ item.name }}</p>
+        <p>{{ item.count }}</p>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'MapBottomBar',
+  data() {
+    return {
+      barData: [
+        {
+          name: '监控设备',
+          count: 80,
+        },
+        {
+          name: '可燃气体',
+          count: 10,
+        },
+        {
+          name: '有毒气体',
+          count: 23,
+        },
+        {
+          name: '未处理告警',
+          count: 0,
+        },
+        {
+          name: '已处理告警',
+          count: 120,
+        },
+        {
+          name: '特殊作业',
+          count: 10,
+        },
+        {
+          name: '隐患排查',
+          count: 10,
+        },
+        {
+          name: '变更',
+          count: 10,
+        },
+        {
+          name: '应急物资',
+          count: 520,
+        },
+      ],
+    }
+  },
+  created() {},
+  // mounted () {},
+  computed: {},
+  watch: {},
+  methods: {},
+}
+</script>
+
+<style lang="less" scoped>
+.map-bottom-bar-container {
+  width: calc(100% - 964px);
+  height: 80px;
+  background: rgba(2, 11, 45, 0.8);
+  position: absolute;
+  bottom: 16px;
+  left: 332px;
+  z-index: 9999;
+  .map-bottom-bar-items {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    justify-content: space-around;
+    .map-bottom-bar-item {
+      p {
+        font-size: 12px;
+        color: #fff;
+        line-height: 40px;
+        text-align: center;
+        margin: 0;
+      }
+    }
+  }
+}
+</style>
